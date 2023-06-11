79fdf162f82953665d57d7cbc6661fdf24a26318
Davidsonnj <davidsoncs45@gmail.com>
2023-06-11 14:53:35 -0300

    Tentando resolver

--- Pasta do davidson/Teste_Davidson.py
+++ Pasta do davidson/Teste_Davidson.py
@@ -1,7 +1,15 @@
 import pickle
-with open('entrada10.bin','rb') as file:
-    l = pickle.load(file)
-    for matricula in l:
-      l[matricula] = nome, semestre, notas, faltas
+def main():
 
-      print(f'{semestre}, {matricula}, {nome},[{notas} {faltas}] ')
+  arquiv = input('Escreva o diretório do arquivo:')
+
+  with open(arquiv,'rb') as file:
+      
+      l = pickle.load(file)
+
+      for matricula in l:
+          nome, semestre, notas, faltas = l[matricula]
+          print(f'{semestre}, {matricula}, {nome},[{notas} {faltas}] ')
+
+if __name__ == '__main__':
+   main()
