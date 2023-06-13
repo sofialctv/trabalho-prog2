dbfc0ba326355600ef8b335ca9101f11970a0158
sofialctv <sofiaalcantaras@outlook.com>
2023-06-12 23:07:23 -0300

    V2

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pickle
 
-# ORDENAÇÃO DOS ALUNOS - Função auxiliar
+# COMPARAÇÃO DOS ALUNOS
 def compara_Alunos(m1, m2, planilha):
 
     nome_Aluno1, semestre_Aluno1, notas_Aluno1, faltas_Aluno1 = planilha[m1]
     nome_Aluno2, semestre_Aluno2, notas_Aluno2, faltas_Aluno2 = planilha[m2]
+        
+    ano1 = semestre_Aluno1[0]
+    ano2 = semestre_Aluno2[0]
 
-    # Aproveitando o nome da variável, apenas estou dando um novo valor a ela. A partir de agora, a var. deixa de ser uma tupla e passa a ser a soma dos nº que continha.
+    periodo1 = semestre_Aluno1[1]
+    periodo2 = semestre_Aluno2[1]
 
-    # notas_Aluno = (notaP1, notaTrabalho, notaMaratona, bonusExercícios)
 
     notas_Aluno1 = sum(notas_Aluno1)
-    notas_Aluno2 = sum(notas_Aluno2)
-
-    # Calculando os pontos extras das faltas & Garantindo que a nota nunca ultrapassará 100 pontos
+    notas_Aluno2 = sum(notas_Aluno2)    
+    
+    # Cálculo dos pontos extras das faltas + Garantindo que a nota nunca ultrapassará 100 pontos
 
     if faltas_Aluno1 == 0 and notas_Aluno1 <= 98: 
         notas_Aluno1 += 2
@@ -25,20 +28,44 @@ def compara_Alunos(m1, m2, planilha):
     elif faltas_Aluno2 == 0 and notas_Aluno2 == 99:
         notas_Aluno2 += 1
 
+# COMPARAÇÕES - Já escritas seguindo a ordem de desempate
 
-# COMPARAR SEMESTRES [...]
+    # Semestres
+    if ano1 > ano2: return True
+    if ano1 < ano2: return False
 
+    if ano1 == ano2:
+        if periodo1 > periodo2: return True
+        if periodo1 < periodo2: return False
 
-# MAIN: Função principal
+    # Nota final
+    if notas_Aluno1 > notas_Aluno2: return True
+    if notas_Aluno1 < notas_Aluno2: return False
+
+    # Nome
+    if nome_Aluno1 > nome_Aluno2: return True
+    if nome_Aluno1 < nome_Aluno2: return False
+
+    # Matrícula
+    if m1 > m2: return True
+    if m1 < m2: return False
+
+# MAIN: Função principal - Abrindo o arquivo de entrada
 def main():
-    # Utilizando método 'with open' p/ otimizar o código: assim, não precisamos lembrar de fechar o arquivo posteriormente
+
+    lista_Matriculas = []                       # Criando lista apenas com as matrículas dos alunos, que são as chaves do dicionário (planilha)
     
     with open('entrada.bin', 'rb') as arq:      # 'arq' é a variável que será usada p/ se referir ao objeto de arquivo
         planilha = pickle.load(arq)             # Lendo o dicionário do arquivo binário com uma único comando
 
-    matriculas = list(planilha.keys())          # Criando lista apenas com as matrículas dos alunos, que são as chaves do dicionário (planilha)
+    for chave in planilha:                      # Lembrando que as chaves do dicionário são as matrículas de cada aluno
+        lista_Matriculas.append(chave)
 
-    # comparaMatricula
+    # ORDENAMatricula
+
+
+
+    # Escrevendo o arquivo em formato txt
 
 
 if __name__ == '__main__':
